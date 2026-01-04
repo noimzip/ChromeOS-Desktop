06ec4229221efef8651e4ec68720e9912dc69e6b
noimzip <gudoumaki@proton.me>
2026-01-04 11:37:08 +0900

    Add Settings Modal

--- desktop.js
+++ desktop.js
@@ -15,6 +15,18 @@ document.getElementById('entry-settings').onclick = () => {
 
 document.addEventListener('keydown', function(e) {
   if(e.key === 'Escape'){
-    
+    escmenu_modal_overlay.style.display = 'block';
   }
-})
+});
+
+document.getElementById('close_menu_modal').onclick = () => {
+  escmenu_modal_overlay.style.display = 'none';
+}
+
+document.getElementById('open_settingsmenu_modal').onclick = () => {
+  settingsmenu_modal_overlay.style.display = 'block';
+}
+
+document.getElementById('close_settingsmenu_modal').onclick = () => {
+  settingsmenu_modal_overlay.style.display = 'none';
+}
