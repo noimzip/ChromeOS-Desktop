742a9be788b4391bea9aa306d149875c2914d1e4
noimzip <gudoumaki@proton.me>
2026-01-04 12:32:45 +0900

    jQuery implementation, enabling icon movement

--- desktop.js
+++ desktop.js
@@ -24,9 +24,25 @@ document.getElementById('close_menu_modal').onclick = () => {
 }
 
 document.getElementById('open_settingsmenu_modal').onclick = () => {
+  escmenu_modal_overlay.style.display = 'none';
   settingsmenu_modal_overlay.style.display = 'block';
 }
 
 document.getElementById('close_settingsmenu_modal').onclick = () => {
   settingsmenu_modal_overlay.style.display = 'none';
+}
+
+document.getElementById('open_change_widget_position_modal').onclick = () => {
+  escmenu_modal_overlay.style.display = 'none';
+  change_widget_position_modal_overlay.style.display = 'block';
+
+  document.getElementById('entry-chrome').onpointermove = function(event){
+    if(event.buttons){
+        this.style.left     = this.offsetLeft + event.movementX + 'px'
+        this.style.top      = this.offsetTop  + event.movementY + 'px'
+        this.style.position = 'absolute'
+        this.draggable      = false
+        this.setPointerCapture(event.pointerId)
+    }
+  }
 }
